e26c84720b8f365d0b53341b5798040a8f75761c
Brian Lee <brianlee1014@gmail.com>
2017-04-11 17:34:45 -0700

    added survey js

--- wic/src/App.js
+++ wic/src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as Survey from 'survey-react';
 
 class App extends Component {
-	constructor(){
+	constructor() {
 		super();
-
+		this.surveyJSON = { title: "Determining Your WIC Eligibility", pages: [
+			{ name:"page1", questions: [
+				{ type: "rating", name: "familySize", title: "How members are in your immediate family?" },
+				{ type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "pregnant",title: "Is anyone currently pregnant?" },
+				{ type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{pregnant} = 'Yes'" }
+			]},
+			{ name: "page2", questions: [
+				{ type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
+				{ type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
+			{ name: "page3",questions: [
+				{ type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+		]
+		}
 	}
+
+	sendDataToServer(survey) {
+		var resultAsString = JSON.stringify(survey.data);
+		alert(resultAsString); //send Ajax request to your web server.
+	};
 
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
+			<Survey.Survey json={this.surveyJSON} onComplete={this.sendDataToServer} />
 		);
 	}
 }
-
-
 
 export default App;
--- wic/src/index.js
+++ wic/src/index.js
@@ -1,62 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import Survey from 'survey-react';
+import App from './App';
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
-var surveyJSON = {
-	title: "Tell us, what technologies do you use?", pages: [
-		{
-			name: "page1", questions: [
-			{
-				type: "radiogroup",
-				choices: ["Yes", "No"],
-				isRequired: true,
-				name: "frameworkUsing",
-				title: "Do you use any front-end framework like Bootstrap?"
-			},
-			{
-				type: "checkbox",
-				choices: ["Bootstrap", "Foundation"],
-				hasOther: true,
-				isRequired: true,
-				name: "framework",
-				title: "What front-end framework do you use?",
-				visibleIf: "{frameworkUsing} = 'Yes'"
-			}
-		]
-		},
-		{
-			name: "page2", questions: [
-			{
-				type: "radiogroup",
-				choices: ["Yes", "No"],
-				isRequired: true,
-				name: "mvvmUsing",
-				title: "Do you use any MVVM framework?"
-			},
-			{
-				type: "checkbox",
-				choices: ["AngularJS", "KnockoutJS", "React"],
-				hasOther: true,
-				isRequired: true,
-				name: "mvvm",
-				title: "What MVVM framework do you use?",
-				visibleIf: "{mvvmUsing} = 'Yes'"
-			}]
-		},
-		{
-			name: "page3", questions: [
-			{type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library"}]
-		}
-	]
-}
-Survey.Survey.cssType = "bootstrap";
 ReactDOM.render(
-	<Survey.SurveyWindow json={surveyJSON} onComplete={[]}/>,
-	document.getElementById("surveyContainer"));
+	<App />,
+	document.getElementById('root')
+);
