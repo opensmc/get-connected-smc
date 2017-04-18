4d9ad5bdef8d6df3eb3e5cf69c9644bd31c3f4b7
Brian Lee <blee@smcoe.org>
2017-04-17 17:47:22 -0700

    works
    it works but it’s fugly

--- wic/src/App.js
+++ wic/src/App.js
@@ -1,191 +1,111 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import * as Survey from 'survey-react';
 class App extends Component {
-  constructor() {
-    super();
-    this.surveyJSON = {
-      pages: [
-        {
-          elements: [
-            {
-              type: "text",
-              isRequired: true,
-              name: "q1",
-              title: "What is your family's annual income?"
-            }
-          ],
-          name: "page1"
-        },
-        {
-          elements: [
-            {
-              type: "rating",
-              isRequired: true,
-              name: "q2",
-              rateValues: [
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8"
-              ],
-              title: "How many members are in your immediate family?"
-            },
-            {
-              type: "radiogroup",
-              choices: [
-                {
-                  value: "Y",
-                  text: "Yes"
-                },
-                {
-                  value: "N",
-                  text: "No"
-                }
-              ],
-              isRequired: true,
-              name: "q3",
-              title: "Are any of your family members currently pregnant?"
-            },
-            {
-              type: "radiogroup",
-              choices: [
-                {
-                  value: "Y",
-                  text: "Yes"
-                },
-                {
-                  value: "N",
-                  text: "No"
-                }
-              ],
-              isRequired: true,
-              name: "q4",
-              title: "Are there children ages 5 and under?"
-            }
-          ],
-          name: "page2"
-        },
-        {
-          elements: [
-            {
-              type: "radiogroup",
-              choices: [
-                {
-                  value: "Y",
-                  text: "Yes"
-                },
-                {
-                  value: "N",
-                  text: "No"
-                }
-              ],
-              isRequired: true,
-              name: "q5",
-              title: "How  you participated in either CalFresh or Medi-Cal in the past 12 months?"
-            },
-            {
-              type: "html",
-              html: "You qualify because you have a child under 5.",
-              name: "y1",
-              visible: false,
-              visibleIf: "{q4} = 'Y'"
-            },
-            {
-              type: "html",
-              html: "You qualify because you participated in either CalFresh or Medi-Cal in the past 12 months.",
-              name: "y2",
-              visible: false,
-              visibleIf: "{q5} = 'Y'"
-            },
-            {
-              type: "html",
-              html: "You qualify because one of your immediate family members is currently pregnant.",
-              name: "y3",
-              visible: false,
-              visibleIf: "{q3} = 'value1'"
-            },
-            {
-              type: "html",
-              html: "Your income qualifies you for aid.",
-              name: "y4",
-              visible: false,
-              visibleIf: " ({q1} < 12061 and {q2} < 2)"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $16,240) qualifies for aid.",
-              name: "y5",
-              visible: false,
-              visibleIf: " ({q1} < 16241 and {q2} < 3)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $20,420) qualifies for aid.",
-              name: "y6",
-              visible: false,
-              visibleIf: " ({q1} < 20420 and {q2} < 4)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $24,600) qualifies for aid.\n",
-              name: "y7",
-              visible: false,
-              visibleIf: " ({q1} < 25601 and {q2} < 5)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $28,780) qualifies for aid.\n",
-              name: "y8",
-              visible: false,
-              visibleIf: " ({q1} < 28781 {q2} < 6)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $32,960) qualifies for aid.",
-              name: "y9",
-              visible: false,
-              visibleIf: " ({q1} < 32961 and {q2} < 7)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $37140) qualifies for aid.",
-              name: "y10",
-              visible: false,
-              visibleIf: " ({q1} < 37141 and {q2} < 8)\n"
-            },
-            {
-              type: "html",
-              html: "Your family's income (less than $41.320) qualifies for aid.\n",
-              name: "y11",
-              visible: false,
-              visibleIf: " ({q1} < 41321 and {q2} < 9)\n"
-            }
-          ],
-          name: "page4"
-        }
-      ]
-    }
-  }
+	constructor() {
+		super();
+		this.surveyJSON = {
+			pages: [
+				{
+					elements: [
+						{
+							type: "text",
+							isRequired: true,
+							name: "q1",
+							title: "What is your family's annual income?"
+						}
+					],
+					name: "page1"
+				},
+				{
+					elements: [
+						{
+							type: "rating",
+							isRequired: true,
+							name: "q2",
+							rateValues: [
+								"1",
+								"2",
+								"3",
+								"4",
+								"5",
+								"6",
+								"7",
+								"8"
+							],
+							title: "How many members are in your immediate family?"
+						},
+						{
+							type: "radiogroup",
+							choices: [
+								{
+									value: "Y",
+									text: "Yes"
+								},
+								{
+									value: "N",
+									text: "No"
+								}
+							],
+							isRequired: true,
+							name: "q3",
+							title: "Are any of your family members currently pregnant?"
+						},
+						{
+							type: "radiogroup",
+							choices: [
+								{
+									value: "Y",
+									text: "Yes"
+								},
+								{
+									value: "N",
+									text: "No"
+								}
+							],
+							isRequired: true,
+							name: "q4",
+							title: "Are there children ages 5 and under?"
+						}
+					],
+					name: "page2"
+				},
+				{
+					elements: [
+						{
+							type: "radiogroup",
+							choices: [
+								{
+									value: "Y",
+									text: "Yes"
+								},
+								{
+									value: "N",
+									text: "No"
+								}
+							],
+							isRequired: true,
+							name: "q5",
+							title: "How  you participated in either CalFresh or Medi-Cal in the past 12 months?"
+						},
+					],
+					name: "page4"
+				}
+			]
+		}
+	}
 
-  sendDataToServer(survey) {
-    var resultAsString = JSON.stringify(survey.data);
-    alert(resultAsString); //send Ajax request to your web server.
-  };
+	processSurveyData(survey) {
+		var resultAsString = JSON.stringify(survey.data);
+		alert(resultAsString); //send Ajax request to your web server.
+	};
 
-  render() {
-    return (
-      < Survey.Survey
-    json = {this.surveyJSON
-  }
-    onComplete = {this.sendDataToServer
-  } />
-  )
-    ;
-  }
+	render() {
+		return (
+			< Survey.Survey
+				json={this.surveyJSON}
+				onComplete={this.processSurveyData}/>
+		);
+	}
 }
 export default App;
--- wic/src/index.js
+++ wic/src/index.js
@@ -1,9 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as Survey from 'survey-react';
 import './index.css';
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
+var surveyJSON = { title: "WIC", showProgressBar: "top", pages: [
+	{questions: [
+		{ type: "text", name: "income", title: "What is your family's annual income?",isRequired: true},
+		{ type: "text", name: "numFamilyMembers", title: "How many members are in your immediate family?",
+			isRequired: true },
+		{ type: "radiogroup", name: "pregnant",
+			title: "Are any of your family members currently pregnant?",
+			choices: [
+				{
+					value: "Y",
+					text: "Yes"
+				},
+				{
+					value: "N",
+					text: "No"
+				}
+			],
+			isRequired: true },
+		{ type: "radiogroup", name: "underFive",
+			title: "Are there children ages 5 and under?",
+			choices: [
+				{
+					value: "Y",
+					text: "Yes"
+				},
+				{
+					value: "N",
+					text: "No"
+				}
+			],
+			isRequired: true },
+		{ type: "radiogroup", name: "isCalFreshMediCal",
+			title: "How  you participated in either CalFresh or Medi-Cal in the past 12 months?",
+			choices: [
+				{
+					value: "Y",
+					text: "Yes"
+				},
+				{
+					value: "N",
+					text: "No"
+				}
+			],
+			isRequired: true },
+	]},
+]};
+
+Survey.Survey.cssType = "bootstrap";
+
+let model = new Survey.Model(surveyJSON);
+model.onComplete.add(function (sender) {
+	//var mySurvey = sender;
+	//var surveyData = sender.data;
+	let respondentIncome = Number.parseInt(model.data.income);
+	let maxIncomeBasedOnNumPeople = 12060 * ((model.data.numFamilyMembers - 1) * 4180) * 1.85;
+	if (respondentIncome <= maxIncomeBasedOnNumPeople){
+		if (model.data.underFive === 'Y') {
+			alert(JSON.stringify('Your family qualifies because of your income and you have a child under 5.'));
+		} else if (model.data.isCalFreshMediCal === 'Y') {
+			alert(JSON.stringify('Your family qualifies because of your income and you participated in CalFresh or MediCal in the last year.'));
+		} else if (model.data.pregnant === 'Y') {
+			alert(JSON.stringify('Your family qualifies because of your income a member of your immediate family is pregnant.'));
+		} else {
+			alert("Sorry. You do not qualify because your family currently does not have a child under 5 and has not participated in CalFresh or Medial in the past year.");
+		}
+	} else {
+		alert("Sorry. You do not qualify due to your income.");
+	}
+
+	// The following doesn't make any sense as s
+
+	//alert(JSON.stringify(model.data));
+});
+
+ReactDOM.render(<Survey.Survey model={model} />, document.getElementById("root"));
