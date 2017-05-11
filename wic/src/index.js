6ea6f36a7a5ef27412a54ecc38c292f6e2b5e527
Jeffrey Fisher <jeff@slofish.io>
2017-05-10 19:31:13 -0700

    Fetches info from smc-connect api based upon zip code entered and ‘food’ keyword and displays the results in the console, which will obviously need to be improved, but it seems like a good start.

--- wic/src/index.js
+++ wic/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Survey from 'survey-react';
+import 'whatwg-fetch'
 import './index.css';
 
 var surveyJSON = { title: "WIC", showProgressBar: "top", pages: [
@@ -47,6 +48,7 @@ var surveyJSON = { title: "WIC", showProgressBar: "top", pages: [
 				}
 			],
 			isRequired: true },
+        { type: "text", name: "zipcode", title: "What zipcode are you in?", isRequired: true},
 	]},
 ]};
 
@@ -56,7 +58,7 @@ let model = new Survey.Model(surveyJSON);
 model.onComplete.add(function (sender) {
 	//var mySurvey = sender;
 	//var surveyData = sender.data;
-	let respondentIncome = Number.parseInt(model.data.income);
+	let respondentIncome = Number.parseInt(model.data.income, 10);
 	let maxIncomeBasedOnNumPeople = 12060 * ((model.data.numFamilyMembers - 1) * 4180) * 1.85;
 	if (respondentIncome <= maxIncomeBasedOnNumPeople){
 		if (model.data.underFive === 'Y') {
@@ -72,9 +74,36 @@ model.onComplete.add(function (sender) {
 		alert("Sorry. You do not qualify due to your income.");
 	}
 
-	// The following doesn't make any sense as s
 
-	//alert(JSON.stringify(model.data));
+	let zipcode = model.data.zipcode;
+
+    fetch('https://api.smc-connect.org/search?keyword=food&location=' + zipcode)
+		.then(function(response) {
+            return response.json();
+        }).then(function(json) {
+			for (let item of json) {
+				console.log(item);
+
+				console.log('Name: ' + item.name);
+
+				for (let phone of item.phones) {
+					console.log('Phone: ' + phone.number);
+				}
+
+				console.log('Address: ' + item.address.address_1 + ' ' + item.address.city + ' ' + item.address.postal_code);
+				console.log('Description: ' + item.description);
+
+				console.log('------------------------------------------------------------------------------------------')
+
+				// Much more info can be extracted from the data ...
+				// TODO: Determine exactly what fields to display from the returned dataset
+				// TODO: Display the data on the page instead in the console
+			}
+
+		}).catch(function(ex) {
+			console.log('parsing failed', ex);
+		})
+
 });
 
 ReactDOM.render(<Survey.Survey model={model} />, document.getElementById("root"));
