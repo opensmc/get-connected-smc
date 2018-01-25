3e06010d47c63f3020382a1ab8dc73aa0b95b132
RohitUdayTalwalkar <RohitTalwalkar@gmail.com>
2018-01-24 22:29:35 -0800

    Info added and some styling and feedback needed

--- wic/src/components/SurveyResults.js
+++ wic/src/components/SurveyResults.js
@@ -35,21 +35,21 @@ class SurveyResults extends React.Component {
         <div>
           <div className='call-forimmediate-help'>
             <p><strong>Need help getting food now?</strong></p>
-            <div>
-              <span>Call Food Connection at 1-800-984-3663</span>
-              <span>Monday - Friday, 8:00 am - 5:00 pm.</span>
-            </div>
-            <div>
-              <span>Staff members speak English, Spanish, Vietnamese, Chinese (Cantonese and Mandarin), and Tagalog. Three-way interpretation is available for other languages.</span>
-              <span>Call 211 after hours</span>
-              <span>24/7, free, confidential, available in multiple languages. Operated by United Way Bay Area.</span>
-            </div>
-            <div>            
-              <span>Email Food Connection at foodconnection@shfb.org.</span>
-            </div>
-            <div>
-              <span>Text "Web Connect" at 408-455-5181</span>
-            </div>
+            <p>
+              <div>Call Food Connection at 1-800-984-3663</div>
+              <div>Monday - Friday, 8:00 am - 5:00 pm.</div>
+            </p>
+            <p>
+              <div>Staff members speak English, divish, Vietnamese, Chinese (Cantonese and Mandarin), and Tagalog. Three-way interpretation is available for other languages.</div>
+              <div>Call 211 after hours</div>
+              <div>24/7, free, confidential, available in multiple languages. Operated by United Way Bay Area.</div>
+            </p>
+            <p>            
+              <div>Email Food Connection at foodconnection@shfb.org.</div>
+            </p>
+            <p>
+              <div>Text "Web Connect" at 408-455-5181</div>
+            </p>
           </div>
           <div className={`card-deck`}>
             { results.map((item) => <Item key={item} message={item} />) }
