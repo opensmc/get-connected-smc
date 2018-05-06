afc8fe7d46a4ecd9168626ee25dcc4f8425e7345
Saad Mir <saadmir@gmail.com>
2018-05-06 23:49:47 +0000

    updated sub-message text Issue:64, Issue:65

--- wic/src/__t.js
+++ wic/src/__t.js
@@ -1,36 +1,36 @@
 const strings = {
   homeDeliveryMeals: {
     key: 'homeDeliveryMeals',
-    msg: 'You may qualify for Home Delivery Meals',
-    sub_msg: 'Older adults',
+    msg: 'You qualify for free lunch at dining sites.',
+    sub_msg: 'Click here to learn more',
     icon: 'icons/noun_744795_cc.png',
     url: 'http://www.smchealth.org/general-information/where-seniors-can-go-nutritious-meals'
   },
   freeSchoolMeals: {
     key: 'freeSchoolMeals',
     msg: 'You may qualify for free school meals',
-    sub_msg: 'School age children',
+    sub_msg: 'Click here to learn more',
     icon: 'icons/noun_1048458_cc.jpg',
     url: 'http://www.smcoe.org/about-smcoe/districts-and-schools/'
   },
   reducedPriceSchoolMeals: {
     key: 'reducedPriceSchoolMeals',
     msg: 'You may qualify for reduced priced meals at school',
-    sub_msg: 'School age children',
+    sub_msg: 'Click here to learn more',
     icon: 'icons/noun_1048458_cc.jpg',
     url: 'http://www.smcoe.org/about-smcoe/districts-and-schools/'
   },
   WIC: {
     key: 'WIC',
     msg: 'You may be WIC eligible',
-    sub_msg: 'Women and Children',
+    sub_msg: 'Click here to learn more',
     icon: 'icons/noun_851501_cc.png',
     url: 'http://www.smchealth.org/wic'
   },
   CalFresh: {
     key: 'CalFresh',
     msg: 'You may qualify for CalFresh',
-    sub_msg: 'Family/food',
+    sub_msg: 'Click here to learn more',
     icon: 'icons/CalFresh.png',
     url: 'https://getcalfresh.org/'
   },
